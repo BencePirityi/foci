1f111202b8363d0d8d8fd06518a4cf6c83606cd3
BencePirityi <bpirityi2@gmail.com>
2022-05-12 12:05:26 +0200

    4. feladat kész, random számok

--- bencepirityi1022.py
+++ bencepirityi1022.py
@@ -1,4 +1,12 @@
-meccsek = [["1. foduló", "2. foduló", "3. foduló", "4. foduló", "5. foduló"], [3, 2, 0, 2, 3], [0, 0, 1, 0, 3]]
+import random
+
+meccsek = [["1. foduló", "2. foduló", "3. foduló", "4. foduló", "5. foduló"], [], []]
+
+for i in range(5):
+    random1 = random.randrange(0, 5)
+    random2 = random.randrange(0, 5)
+    meccsek[1].append(random1)
+    meccsek[2].append(random2)
 
 print("1. feladat - Eredmények:\n")
 for i in range(len(meccsek[0])):
@@ -8,8 +16,19 @@ atlagosgol = (sum(meccsek[1]) + sum(meccsek[2])) / (len(meccsek[1]) + len(meccse
 print("\n2. feladat")
 print(f"A meccseken átlagosan {atlagosgol} gól született.")
 
-print("\n 3. feladat")
+print("\n3. feladat")
 if sum(meccsek[1]) > sum(meccsek[2]):
     print("A hazai csapat győzőtt többszőr.")
 else:
     print("A vendég csapat győzött többszőr.")
+
+print("\n4. feladat")
+for i in range(len(meccsek[0])):
+    if meccsek[1][i] == meccsek[2][i]:
+        dontetlenmeccs = True
+    else:
+        dontetlenmeccs = False
+if dontetlenmeccs:
+    print("Volt döntetlen mérkőzés")
+else:
+    print("Nem volt döntetlen mérkőzés")
