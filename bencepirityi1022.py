6fefaa9292e464d0afc8909a2caf974355b2d821
BencePirityi <bpirityi2@gmail.com>
2022-05-12 11:34:11 +0200

    2. feladat kész

--- bencepirityi1022.py
+++ bencepirityi1022.py
@@ -3,3 +3,7 @@ meccsek = [["1. foduló", "2. foduló", "3. foduló", "4. foduló", "5. foduló"
 print("1. feladat - Eredmények:\n")
 for i in range(len(meccsek[0])):
     print(f"{meccsek[0][i]}: {meccsek[1][i]}:{meccsek[2][i]}")
+
+atlagosgol = (sum(meccsek[1]) + sum(meccsek[2])) / (len(meccsek[1]) + len(meccsek[2]))
+print("\n2. feladat")
+print(f"A meccseken átlagosan {atlagosgol} gól született.")
